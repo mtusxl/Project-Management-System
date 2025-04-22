3011c17e9c93db13bbe56ba5ac9dbec68f8c83d1
mtusxl <mateusru54063@gmail.com>
2025-04-22 19:42:12 +0300

    moved the project creation function to the Project application

--- Project_Menegement_System/Projects/urls.py
+++ Project_Menegement_System/Projects/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('project/<slug:slug>/update_project/', views.update_project, name='update_project'),
     path('project/<slug:slug>/', views.project_list, name='project'),
+    path('create_project/', views.Create_project, name='create_project'),
     path('project/<slug:slug>/delete/', views.delete_project, name='delete_project'),
+    
+   
 
     #path('<int:id>/', views.project_detail, name='project_detail'),
 ]
--- Project_Menegement_System/Projects/views.py
+++ Project_Menegement_System/Projects/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from .models import Project
+from Tasks.models import Task
 
 @login_required
 def project_list(request, slug):
@@ -45,3 +46,43 @@ def delete_project(request, slug):
     if request.method == 'POST':
         project.delete()
         return redirect('home')
+
+
+@login_required
+def Create_project(request):
+    if request.method=="POST":
+        print(f"project_name - {request.POST["project_name"]}")
+        slug = slugify(request.POST["project_name"])
+        print(f"Slug - {slug}")
+        name=request.POST.get("project_name")
+        description = request.POST.get("project_description")
+        members = request.POST.get("members")
+        print(f"members {members}")
+        
+        project = Project(
+            name = name,
+            description = description,
+            author=request.user,
+            slug=slug
+        )
+        project.save()
+        project.members.add(request.user)
+        if members:
+            project.members.add(*members)
+        request.user.projects.add(project)
+        task_count = int(request.POST.get('task_count', 0))
+        for i in range(1, task_count + 1):
+            task_title = request.POST.get(f'task_title_{i}')
+            task_description = request.POST.get(f'task_description_{i}')
+            if task_title:
+                task = Task(
+                    name=task_title,
+                    description=task_description,
+                    project=project,
+                    assigned_to = request.user
+
+                )
+                task.save()
+
+        return redirect(f'/project/{slug}/')
+    return  render(request, 'project_form.html')
