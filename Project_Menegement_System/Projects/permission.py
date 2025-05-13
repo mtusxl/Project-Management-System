3ca908dafb667570d0405dd5ae93a5e15f3c8237
mtusxl <mateusru54063@gmail.com>
2025-05-13 14:28:25 +0300

    Update permission class

--- Project_Menegement_System/Projects/permission.py
+++ Project_Menegement_System/Projects/permission.py
@@ -2,6 +2,11 @@ from rest_framework import permissions
 
 class ProjectPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        print(f"User: {request.user}, Author: {obj.author}, Method: {request.method}")
+
+        if request.user == obj.author:
             return True
-        return obj.author == request.user
+
+        if request.method in permissions.SAFE_METHODS:
+            return request.user == obj.author or request.user in obj.members.all()
+        return obj.author.username == request.user
