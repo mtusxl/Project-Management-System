492a1a0bbbb48cb39615922eaeb5c553f3225cd0
mtusxl <mateusru54063@gmail.com>
2025-05-21 13:46:34 +0300

    Added new 3 fields in array 'data'

--- Project_Menegement_System/Projects/views.py
+++ Project_Menegement_System/Projects/views.py
@@ -61,27 +61,30 @@ class project_detailAPI(APIView):
             "tasks": [
                 {                                   
                     "id": task.id,
-                    "name": task.name,  # Обратите внимание - в шаблоне ожидается name, а не title
+                    "name": task.name,  
                     "status": task.status,
                     "assigned_to": {
                         "id": task.assigned_to.id,
                         "username": task.assigned_to.username
                     } if task.assigned_to else None,
-                    # добавьте другие поля задачи по необходимости
+                   
                 } for task in tasks
             ],
             "tasks_todo": [
                 {"id": task.id, "name": task.name} 
                 for task in tasks_todo
             ],
+            "count_todo": len(tasks_todo),
             "tasks_in_progress": [
                 {"id": task.id, "name": task.name} 
                 for task in tasks_in_progress
             ],
+            "count_in_progress": len(tasks_in_progress),
             "tasks_done": [
                 {"id": task.id, "name": task.name} 
                 for task in tasks_done
-            ]
+            ],
+            "count_done":len(tasks_done)
         }
         
         return Response(data, status=status.HTTP_200_OK)
@@ -116,85 +119,3 @@ class ProjectAPI(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         else: return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @login_required
-# def project_list(request, slug):
-
-#     project = get_object_or_404(Project, slug=slug)
-#     tasks = project.tasks.filter(assigned_to=request.user)
-#     tasks_todo = tasks.filter(status="to_do")
-#     tasks_in_progress = tasks.filter(status="in_progress")
-#     tasks_done = tasks.filter(status="done")
-    
-   
-#     return render(request, "project.html", {"project":project,
-#                                             "tasks":tasks,
-#                                             "tasks_todo":tasks_todo,
-#                                             "tasks_in_progress":tasks_in_progress,
-#                                             "tasks_done":tasks_done})
-
-
-# def update_project(request, slug):
-#     if request.method == 'POST':
-#         print("Вьюха для обновления")
-#         project = get_object_or_404(Project, slug=slug)
-#         if project.author != request.user:
-#             return render(request, "project.html", status=403)
-
-#         new_description = request.POST.get("description", "")
-#         if new_description:
-#             project.description = new_description
-#             project.save()
-#             print("Описание обновлено")
-#             return redirect(f"/project/{project.slug}")
-    
-# @login_required
-# def delete_project(request, slug):
-#     project = get_object_or_404(Project, slug=slug)
-#     if project.author != request.user:
-#         return JsonResponse({'error': 'Access denied'}, status=403)
-
-#     if request.method == 'POST':
-#         project.delete()
-#         return redirect('home')
-
-
-# @login_required
-# def Create_project(request):
-#     if request.method=="POST":
-#         print(f"project_name - {request.POST["project_name"]}")
-#         slug = slugify(request.POST["project_name"])
-#         print(f"Slug - {slug}")
-#         name=request.POST.get("project_name")
-#         description = request.POST.get("project_description")
-#         members = request.POST.get("members")
-#         print(f"members {members}")
-#
-#         project = Project(
-#             name = name,
-#             description = description,
-#             author=request.user,
-#             slug=slug
-#         )
-#         project.save()
-#         project.members.add(request.user)
-#         if members:
-#             project.members.add(*members)
-#         request.user.projects.add(project)
-#         task_count = int(request.POST.get('task_count', 0))
-#         for i in range(1, task_count + 1):
-#             task_title = request.POST.get(f'task_title_{i}')
-#             task_description = request.POST.get(f'task_description_{i}')
-#             if task_title:
-#                 task = Task(
-#                     name=task_title,
-#                     description=task_description,
-#                     project=project,
-#                     assigned_to = request.user
-
-#                 )
-#                 task.save()
-
-#         return redirect(f'/project/{slug}/')
-#     return  render(request, 'project_form.html')
